f3480c3507d4d979ad8e581055eaf90349b17e41
khairi <khairi.yusoff@qumonintelligence.com>
2023-11-16 14:39:41 +0800

    Created Form2.js

--- antd-learning/src/App.js
+++ antd-learning/src/App.js
@@ -1,9 +1,10 @@
 import Form1 from "./Data Entry/Form/Form1";
-
+import Form2 from "./Data Entry/Form/Form2";
 function App() {
   return (
     <>
       <Form1 />
+      <Form2 />
     </>
   );
 }
