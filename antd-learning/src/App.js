ac8987c5154b90f48c6aa9b22c5d63228af35fa3
khairi <khairi.yusoff@qumonintelligence.com>
2023-11-26 00:44:59 +0800

    update

--- antd-learning/src/App.js
+++ antd-learning/src/App.js
@@ -2,13 +2,11 @@ import Form1 from "./Data Entry/Form/Form1";
 import Form2 from "./Data Entry/Form/Form2";
 import Input1 from "./Data Entry/Input/Input1";
 import Autocomplete1 from "./Data Entry/Autocomplete/Autocomplete1";
+import Space1 from "./Layout/Space1";
 function App() {
   return (
     <>
-      <Form1 />
-      <Form2 />
-      <Input1 />
-      <Autocomplete1 />
+      <Space1 />
     </>
   );
 }
